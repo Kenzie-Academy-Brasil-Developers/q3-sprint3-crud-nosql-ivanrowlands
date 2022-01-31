04e3e90047da7d28c8c7b0006d7723701ebabcc0
ivan <ivan.rowlands.macedo@gmail.com>
2022-01-31 12:26:41 -0300

    feature: files squemes and routes done

---
+++ app/routes/post_route.py
@@ -0,0 +1,23 @@
+from app.controllers import post_controller
+
+def post_route(app):
+
+	@app.get("/posts")
+	def read_posts():
+		return post_controller.get_posts()
+
+	@app.get("/posts/<post_id>")
+	def read_post_by_id(post_id):
+		return post_controller.get_post_by_id(post_id)
+	
+	@app.post("/posts")
+	def create_post():
+		return post_controller.create_new_post()
+
+	@app.delete("/posts/<post_id>")
+	def delete_post(post_id):
+		return post_controller.dell_post(post_id)
+
+	@app.patch("/posts/<post_id>")
+	def update_post(post_id):
+		return post_controller.patch_post(post_id)
